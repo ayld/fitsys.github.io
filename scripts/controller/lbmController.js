d8125154a7cb2a77ae9ad36d2e891a8881c6e5d6
Yavor Grancharov <bearhold@gmail.com>
2018-06-08 16:09:57 +0300

    up

--- scripts/controller/lbmController.js
+++ scripts/controller/lbmController.js
@@ -8,31 +8,6 @@ let lbmController = (() => {
       let ankle = globalInfo.getSelectedVal(document.getElementById('ankle-select'));
       let fat = document.getElementById('fat-select');
 
-      let heightRelativeLbm = sex === 'Male'
-          ? lbmModel.heightRelativeLBM(height, 0.973, 99.515)
-          : lbmModel.heightRelativeLBM(height, 0.861, 91.556);
-
-      let heightRelativeWrist = sex === 'Male'
-          ? lbmModel.heightRelativeWrist(height, 0.048, 7.954)
-          : lbmModel.heightRelativeWrist(height, 0.043, 7.22);
-
-      let heightRelativeAnkle = sex === 'Male'
-          ? lbmModel.heightRelativeAnkle(height, 0.059, 12.181)
-          : lbmModel.heightRelativeAnkle(height, 0.083, 5.667);
-
-      let heightLbm = Math.round(heightRelativeLbm);
-      let wristLbm = lbmModel.wristLbm(wrist, heightRelativeWrist, heightRelativeLbm);
-      wristLbm = wristLbm === Infinity || wristLbm === -Infinity ? '' : Math.round(wristLbm);
-      let ankleLbm = lbmModel.ankleLbm(ankle, heightRelativeAnkle, heightRelativeLbm);
-      ankleLbm = ankleLbm === Infinity || ankleLbm === -Infinity ? '' : Math.round(ankleLbm);
-
-      let bodyHeight = lbmModel.fatPercentBodyWeight(fat, heightLbm);
-      bodyHeight = Math.round(bodyHeight);
-      let bodyWrist = lbmModel.fatPercentBodyWeight(fat, wristLbm);
-      bodyWrist = Math.round(bodyWrist);
-      let bodyAnkle = lbmModel.fatPercentBodyWeight(fat, ankleLbm);
-      bodyAnkle = Math.round(bodyAnkle);
-
       let user = {
         id: id,
         name: name,
@@ -40,13 +15,13 @@ let lbmController = (() => {
         height: height,
         wrist: wrist,
         ankle: ankle,
-        heightLbm: heightLbm,
-        wristLbm: wristLbm,
-        ankleLbm: ankleLbm,
+        heightLbm: lbmService.heightLbm(),
+        wristLbm: lbmService.wristLBM(),
+        ankleLbm: lbmService.ankleLBM(),
         fatPercent: fat,
-        bodyWrist: bodyWrist,
-        bodyAnkle: bodyAnkle,
-        bodyHeight: bodyHeight,
+        bodyWrist: lbmService.bodyWrist,
+        bodyAnkle: lbmService.bodyAnkle,
+        bodyHeight: lbmService.bodyHeight,
       };
 
       await lbmHelper.displayLBM(user);
